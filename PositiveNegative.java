848d1172bf6c5e5c68f34ba182682e9aae571944
jegabiuma <147307730+jegabiuma@users.noreply.github.com>
2023-10-13 11:36:35 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,27 @@
+package week1.day2;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		
+		int num=0;
+		if (num<0) {
+			
+			System.out.println(num+ " is negative");
+			
+		}
+		else if (num>0) {
+			
+			System.out.println(num+ " is positive");
+			
+		} 
+		else
+			
+		
+		{
+			System.out.println(num+ " number is neither positive nor negative");
+		}
+
+	}
+
+}
