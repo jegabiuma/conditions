1773fbb9a6bea986f43d34708461c3e383d80caa
jegabiuma <147307730+jegabiuma@users.noreply.github.com>
2023-10-13 11:33:30 +0530

    Add files via upload

---
+++ SecondLargestNumber.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class SecondLargestNumber {
+	
+	public static void main(String[] args) {
+		
+		int[] i= {23,4,67,32,89,22};
+		
+				Arrays.sort(i);
+		System.out.println(i[i.length-2]); 
+			
+			
+		}
+
+		
+	}
+
+
